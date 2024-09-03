6d6a74ede50d9174b8eba2ed2ad754356e3e95fc
CESAR <CESAR@SYSTEMGUSTAVO>
2024-09-02 22:36:53 -0500

    Validacion con Jquery Validate

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,9 +1,14 @@
-﻿namespace ExamenCesarJuñoAjaxNetCoreMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamenCesarJuñoAjaxNetCoreMVC.Models
 {
     public class Producto
     {
         public int ProductoID { get; set; }
+        [Required(ErrorMessage = "La Descripcion es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres")]
         public string Descripcion { get; set; }
+        [Required(ErrorMessage = "El PrecioVenta es obligatorio")]
         public decimal PrecioVenta { get; set; }
         public int StockMinimo { get; set; }
         public int StockActual { get; set; }
---
+++ wwwroot/js/Validacion.js
@@ -0,0 +1,42 @@
+﻿function Validate()
+{
+    console.log('validando !!');
+    $("#createProductForm").validate({
+        rules: {
+            descripcion: {
+                required: true,
+                minlength: 3
+            },
+            precioVenta: {
+                required: true,
+                number: true,
+                min: 0
+            },
+            stockMinimo: {
+                required: true,
+                number: true,
+                min: 0
+            },
+            stockActual: {
+                required: true,
+                number: true,
+                min: 0
+            },
+            fechaVenta: {
+                required: true,
+                date: true
+            },
+            categoria: {
+                required: true
+            }
+        },
+        messages: {
+            descripcion: "Por favor, ingrese una descripción válida.",
+            precioVenta: "El precio de venta debe ser un número positivo.",
+            stockMinimo: "El stock mínimo debe ser un número positivo.",
+            stockActual: "El stock actual debe ser un número positivo.",
+            fechaVenta: "Por favor, seleccione una fecha válida.",
+            categoria: "Debe seleccionar una categoría."
+        }
+    });
+}
